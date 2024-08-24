0482865de6417790fad26946e2f7a1c929a3adac
Tim Bergling <timbergling65535@gmail.com>
2024-08-24 18:56:02 +0700

    first commit for uart lib

---
+++ MiniC/PFS173/UART/UART/pdk_uart.c
@@ -0,0 +1,118 @@
+﻿/*
+ * pdk_uart.c
+ * Created: 2024/08/24
+ * Author: taivb
+ */
+
+#include "extern.h"
+#include "include.h"
+#include "pdk_uart.h"
+
+static BYTE UART_Data_In;
+
+void UART_Send(uint8_t UART_Data_Out)
+{
+	BYTE cnt;
+	//BYTE UART_Data_Out;
+
+	UART_Data_Out = A;
+
+	// Start Bit
+	set0 UART_Out;			// 1
+
+#if FPPA_Duty == 1
+	cnt = 8;				// 2 ~ 3
+	.Delay 3;				// 4 ~ 6
+	do
+	{ // Data Bit * 8
+		.Delay UART_Delay - 10;
+		sr UART_Data_Out;	// 7
+		if (CF)
+		{
+			nop;			// 10
+			UART_Out = 1;	// 1
+		}
+		else
+		{
+			UART_Out = 0;	// 1
+			.delay 2;		// 2 ~ 3
+		}
+	} while (--cnt); 		// 4 ~ 6
+	.Delay UART_Delay - 5;
+#else
+	.Delay UART_Delay - 4;
+	cnt = 8;				// 2 ~ 3
+
+	// Data Bit * 8
+	do
+	{
+		sr		UART_Data_Out;	// 4  4
+		swapc	UART_Out;		// 1
+		.Delay	UART_Delay - 4;
+	} while (--cnt);			// 2, 3
+
+	.Delay 2;					// 3 ~ 4
+#endif
+
+	// Stop Bit
+	set1 UART_Out; // 1
+	.Delay 2 * UART_Delay - 2;
+} // 2
+
+void UART_Receive(void)
+{
+	BYTE cnt;
+
+	while (1)
+	{
+		cnt = 8;
+
+		if (!UART_In)
+		{
+		err: // receive UART error, so ...;
+			continue;
+		}
+		// Wait Start Bit
+		while (UART_In)
+			NULL;
+
+#if FPPA_Duty == 1
+		.Delay(UART_Delay / 2) - 2;
+		if (UART_In)
+			goto err;			// 1, 2
+		.Delay UART_Delay - 3;
+		CF = 0;					// 3
+		do
+		{
+			t0sn UART_In;		// 1
+			CF = 1;				// 2
+			src UART_Data_In;	// 3
+			.Delay UART_Delay - 6;
+		} while (--cnt);		// 4 ~ 6
+#else
+		.Delay UART_Delay / 2;
+		if (UART_In)
+			goto err;			// 1
+
+		.Delay UART_Delay - 1;
+		do
+		{
+			swapc UART_In;		// 1
+			src UART_Data_In	// 2
+			.Delay UART_Delay -	4;
+		} while (--cnt);		// 3, 4
+#endif
+
+		A = UART_Data_In;		// 4
+		// Check Stop Bit
+		if (!UART_In)
+			goto err;
+		return;
+	}
+}
+
+void	UART_begin(void)
+{
+	$ UART_In In;
+	$ UART_Out High, Out;
+}
---
+++ MiniC/PFS173/UART/UART/pdk_uart.h
@@ -0,0 +1,51 @@
+﻿/*
+ * pdk_uart.h
+ * Created: 2024/08/24
+ * Author: taivb
+ */
+
+Baud_Rate => 19200;
+
+UART_Out BIT PA.7;
+UART_In BIT PA.6;
+
+/*
+	UART_Delay => (System_Clock / FPPA_Duty) / Baud_Rate;
+
+	if System_Clock = 8,000.000 Hz
+	FPPA_Duty  = /16
+	so FPPA_Clock  = System_Clock / FPPA_Duty = 500.000 Hz
+
+	if Baud_Rate  = 19200
+	so UART_Delay  = 500.000 / 19200 = 26.0416...
+	so match, 26 cycles send one bit. < 0.5%
+
+	if Baud_Rate  = 38400
+	so UART_Delay  = 500.000 / 38400 = 13.02083...
+	so match, 13 cycles send one bit. < 0.5%
+
+	if Baud_Rate  = 56000
+?	so UART_Delay  = 500.000 / 56000 = 8.9285...  < 1.0%
+
+	if Baud_Rate  = 57600
+X	so UART_Delay  = 500.000 / 57600 = 8.6805...  : fail
+*/
+
+FPPA_Duty => _SYS(INC.FPPA_NUM);	// Single FPPA = 1, Mult FPPA = 2 or 4/8/...
+.errnz 		FPPA_Duty > 2			// Only for 1 /2 FPPA, not for more FPPAs
+
+UART_Delay => ((System_Clock / FPPA_Duty) + (Baud_Rate / 2)) / Baud_Rate;
+// + (Baud_Rate/2) : to round up or down
+
+Test_V0 => System_Clock / 1000 * 995;
+Test_V1 => UART_Delay *Baud_Rate *FPPA_Duty;
+Test_V2 => System_Clock / 1000 * 1005;
+
+#if (Test_V1 < Test_V0) || (Test_V1 > Test_V2)
+	.echo % Test_V0 <= % Test_V1 <= % Test_V2
+	.error Baud_Rate do not match to System Clock
+#endif
+
+void UART_Send(uint8_t UART_Data_Out);
+void UART_Receive(void);
+void UART_begin(void);
